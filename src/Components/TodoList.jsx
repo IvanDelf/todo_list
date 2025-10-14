1f74217ace8875f5425751737ca5e0b182394232
IvanDelf <argoteivandelf@gmail.com>
2025-10-15 00:03:29 +0800

    first commit

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,7 +2,37 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+  const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+
+  const handleAddTodo = () => {
+    const heading = headingInput.trim();
+    if (heading !== '') {
+      setTodos((prev) => [...prev, { heading, lists: [] }]);
+      setHeadingInput('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+   const newTodos = [...todos];
+   newTodos.splice(index, 1);
+   setTodos(newTodos);
+  };
+
+
+  const handleAddList = (index) => {
+   if (listInputs[index] && listInputs[index].trim() !== '') {
+      const newTodos = [...todos];
+      newTodos[index].lists.push(listInputs[index]);
+      setTodos(newTodos);
+      setListInputs({ ...listInputs, [index]: '' });
+    }
+  };
+
+  const handleListInputChange = (index, value) => {
+    setListInputs({ ...listInputs, [index]: value });
+  };
 
   return (
     <>
